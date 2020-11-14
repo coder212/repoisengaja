7dabe2dea8a2dd200734206b4b5589f10e25f901
arbaun zuhdi maarif <yujimaarif.ym@gmail.com>
2020-11-14 23:48:09 +0700

    kalo mau pake nanam dan mancing ddh tinggal uncomment aja itu createloopnya

--- botmancingddh.py
+++ botmancingddh.py
@@ -1,7 +1,7 @@
 #dibuat oleh john thor 
 #kalo mau pake ya pake aja sih kalo pengen mau ngembangin juga silakan
 #yang jelas ini source code hanyalah asal ketik
-#library yan diperlukan adalah telethon sisanya udah bawaan dan aku mengunakan bot ini dengan python3.5
+#library yang diperlukan adalah telethon sisanya udah bawaan dan aku mengunakan bot ini dengan python3.5
 import os, re, random, asyncio, sys, time, json
 from telethon import TelegramClient, events, sync
 def read_the_env():
@@ -14,20 +14,37 @@ configjson = json.loads(read_the_env())
 API_ID = configjson['api_id']
 API_HASH = configjson['api_hash']
 client = TelegramClient('makun', API_ID, API_HASH)
-dest= 'danaudalamhutan'
+
+dest= ('danaudalamhutan','kampungmaifambot')
+
 
 async def nungguin(w):
-   await asyncio.sleep(w*60)
+   await asyncio.sleep(w)
+
+
+async def bertani(client,w, tanaman, lahan):
+   while True:
+       await client.send_message(dest[1], "/panen")
+       await nungguin(1)
+       await client.send_message(dest[1], "/tanam_{}_{}".format(tanaman, lahan))
+       await nungguin(1)
+       await client.send_message(dest[1], "/siram")
+       await nungguin(w)
 
 async def mancingddh(client,w):
    while True:
-       await client.send_message(dest, "/fish")
+       await client.send_message(dest[0], "/fish")
        await nungguin(w)
 
-#@client.on(events.NewMessage(incoming=True, from_users=dest))
-#async def handle_chat(event):
-#   message = event.raw_text
-#   print(message)
+async def cekstatus(client,w):
+   while True:
+       await client.send_message(dest[1], "/status")
+       await nungguin(w)
+
+@client.on(events.NewMessage(incoming=True, from_users=dest))
+async def handle_chat(event):
+   message = event.raw_text
+   print(message)
 #   if "halo" in message:
 #      await client.send_message(dest, "oh iya")
 #   else:
@@ -36,5 +53,7 @@ async def mancingddh(client,w):
 #      await client.send_message(dest, jrandom[a])
 
 with client:
-   client.loop.create_task(mancingddh(client,8))
+   #client.loop.create_task(mancingddh(client,480))
+   #client.loop.create_task(bertani(client,300,"cabai",143))
+   client.loop.create_task(cekstatus(client,300))
    client.run_until_disconnected()
